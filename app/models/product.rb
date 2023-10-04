a89d456bb8a01d36223eaf0bc4fad5b28a5195c0
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-10-04 15:14:48 -0500

    add validation to all models

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   belongs_to :brand
   has_many :product_tags
   has_many :tags, through: :product_tags
+  validates :name, :price, presence: true
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ApplicationRecord
   has_many :product_tags
   has_many :products, through: :product_tags
+  validates :name, presence: true, uniqueness: true
 end
