668cb1e988ff8e5e345d88970c61e0484b557f3c
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-10-04 16:00:11 -0500

    Fetch Makeup API and Populate data to category, brand, and product tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "net/http"
+require "json"
+
+Category.delete_all
+Brand.delete_all
+Tag.delete_all
+Product.delete_all
+ProductTag.delete_all
+
+# Fetch API to populate data to datbase
+url = "http://makeup-api.herokuapp.com/api/v1/products.json"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+data = JSON.parse(response)
+
+data.each do |product_data|
+  category = Category.find_or_create_by(name: product_data["product_type"])
+  brand = Brand.find_or_create_by(name: product_data["brand"])
+
+  if category && category.valid?
+    product = category.products.create(
+      name:        product_data["name"],
+      price:       product_data["price"],
+      image_link:  product_data["image_link"],
+      description: product_data["description"],
+      brand_id:    brand.id
+    )
+  else
+    puts "Invalid category #{product_data['product_type']}"
+  end
+end
