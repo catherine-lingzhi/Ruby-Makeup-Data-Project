c7afc31fc74ac8684346fd7268bb84dcb5994bc3
Lingzhi Luo <lingzhiluo8@outlook.com>
2023-10-18 12:30:45 -0500

    Add if conditional in ERB

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
   root "products#index"
+
   resources :products, only: %i[index show] do
     collection do
       get "search", to: "products#search"
